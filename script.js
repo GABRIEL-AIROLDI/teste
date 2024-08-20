1a3736fec47e539f3f4e8ce3b5a597990258b5d8
Gabriel Henrique  Martins Airoldi <84611383091@senacrs.edu.br>
2024-08-20 09:17:38 -0300

    layout grid

--- script.js
+++
@@ -1,8 +0,0 @@
-
-alert("get out of my computer ")
-document.getElementById("demo").inner.HTML ="get of now";
-let nome = prompt("digite seu nome")
-alert("olá "+nome+",tudo bem")
-// console edge 
-console. log("get out of my visual studio code ");
-Math.random()*10
